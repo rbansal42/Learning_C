8de04656216a58cee3ac6912b1ceb941a2e14cb4
Rahul <42.rahulbansal@gmail.com>
2021-05-29 01:02:05 +0530

    This is 15

---
+++ Mid-term/q5.c
@@ -0,0 +1,22 @@
+// S = 1 + x*(1/4) + x * (2/8) + ..n terms
+
+#include <stdio.h>
+
+int main() {
+
+    int n, x; 
+    int total=1, sum;
+
+    printf("Input the value for x: ");
+    scanf("%d", &x);
+
+    printf("Input the no of terms n: ");
+    scanf("%d", &n);
+    
+    for(int i=0; i<n; i++) {
+
+
+
+
+    }
+}
