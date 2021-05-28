0262bb3adeef1773cfead5a2353364309f296021
Rahul <42.rahulbansal@gmail.com>
2021-05-29 01:37:05 +0530

    Started Q6

---
+++ Mid-term/q6.c
@@ -0,0 +1,24 @@
+// Q Statement: S = x - (x^2)/2! + (x^3)3! - (x^4)/4! ... for n terms
+
+#include <stdio.h>
+
+int main() {
+
+    float x, n;
+    float s, total;
+    
+    printf("Enter the value of x: ");
+    scanf("%f", &x);
+    
+    printf("Input the no of terms n: ");
+    scanf("%f", &n);
+
+    total = x;
+
+    for(float i = 1; i<=n; i++) {
+
+        
+    }
+
+
+}
