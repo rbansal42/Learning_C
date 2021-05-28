d8e00d58a565aac8ef8d384c01a570ce8942d042
Rahul <42.rahulbansal@gmail.com>
2021-05-29 02:06:10 +0530

    Still trying

--- Mid-term/q6.c
+++ Mid-term/q6.c
@@ -1,11 +1,12 @@
-// Q Statement: S = x - (x^2)/2! + (x^3)3! - (x^4)/4! ... for n terms
+// Q Statement: S = x - (x^2)/2! + (x^3)/3! - (x^4)/4! ... for n terms
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 
     float x, n;
-    float s, total;
+    float s, total=0, fac=1;
     
     printf("Enter the value of x: ");
     scanf("%f", &x);
@@ -13,12 +14,26 @@ int main() {
     printf("Input the no of terms n: ");
     scanf("%f", &n);
 
-    total = x;
+    total = 0;
 
-    for(float i = 1; i<=n; i++) {
+    for(int i=1; i<=n; i++) {
 
+        // Factorial Determination
+        fac = fac * i;
+
+
+        // Finding Value for nth term
+
+        s = x/(fac);
         
+
+        total = total + s;
+
+        x = x * -x;      // x will start off as x^1, but as the loop iterates, it will power up with each iteration as needed for the program
+
     }
+
+    printf("%f", total);
 
 
 }
