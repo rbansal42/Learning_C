8d13249c4042e1549a321ca2710bc73b4cc39547
Rahul <42.rahulbansal@gmail.com>
2021-05-29 22:12:23 +0530

    Increased complexity to train

---
+++ Learning/7. EvenorNot_1.c
@@ -0,0 +1,12 @@
+// Write a Program to perform arithmetic operations on 2 numbers and determine whether the result is even. 
+// Take the numbers from user.
+//
+//  1. sum of the numbers.
+//  2. difference of the numbers.
+//  3. product of the 2 numbers.
+//  4. divisor of the 2 numbers.
+// 
+//  The output should state what is being printed. Ex. "The sum is: _ and the no is Prime."
+// 
+// 
+
