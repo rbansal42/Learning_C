76f5460f1c71b535d25a4a72417b3f95f263a8c2
Rahul <42.rahulbansal@gmail.com>
2021-05-30 17:02:33 +0530

    It's not done, but it's something still

--- class/2dArray/symmetric.c
+++ class/2dArray/symmetric.c
@@ -1,4 +1,7 @@
 // Q. Statement: Find if the given matrix is symmetrical
+//
+//  Matrix A is Symmetric if, the transpose of Matrix A == Matrix A
+// 
 
 #include <stdio.h>
 
@@ -47,4 +50,22 @@ int main() {
     }
 
     printf("\n");
+
+    // Checking if the Matrix is symmetric
+    // The first step would be to see if the no. of rows == no. of columns in the Matrix as that is needed
+    //      for the Matrix to be symmetric.
+
+    if(row != col) {
+
+        printf("As the no. of rows != no. of columns, the given matrix is asymmetric.");
+        return 0;
+    }
+
+    // If the above statement is no true, the program should check each of the indivudal values to see if the Matrix is symmetric
+
+    else {
+
+
+    }
+    
 }
