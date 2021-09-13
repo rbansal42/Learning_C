9bd920381f5043828313a879081c22ada0743eae
Rahul Bansal <rahulbansal.btech20@sushantuniversity.edu.in>
2021-09-13 22:08:09 +0530

    working

--- class/pointers/ptrStringCase.c
+++ class/pointers/ptrStringCase.c
@@ -2,37 +2,37 @@
 
 #include <stdio.h>
 
-toLowerCase(char *s) {
+void toLowerCase(char *s) {
 
     for(int i=0; s[i] != '\0'; i++) {
 
-        if('a' <= s[i] && 'z' >= s[i]) 
+        if('A' <= s[i] && 'Z' >= s[i]) 
         s[i] = s[i] + ('a' - 'A');
     
     }
 }
 
-toUpperCase(char *s) {
+void toUpperCase(char *s) {
 
     for(int i=0; s[i] != '\0'; i++) {
 
-        if('A' <= s[i] && 'Z' >= s[i]) 
+        if('a' <= s[i] && 'z' >= s[i]) 
         s[i] = s[i] - ('a' - 'A');
     
     }
 }
 
-void main() {
+int main() {
 
     char str[30], *ptr;
-    ptr = &str;
+    ptr = str;
 
     int fnc;
 
     printf("Enter a string: ");
     scanf("%s", str);
 
-    printf("\nInput: %s", *ptr);
+    printf("\nInput: %s", ptr);
 
     printf("\n\nWould you like to convert the string to lower case or upper case?");
     printf("\nInput '1' for Upper Case and '2' for Lower Case.");
@@ -41,16 +41,16 @@ void main() {
     scanf("%d", &fnc);
 
     if (fnc == 1) {
-        toUpperCase(&str);
+        toUpperCase(str);
     }
     else if (fnc == 2) {
-        toLowerCase(&str);
+        toLowerCase(str);
     }
     else {
         printf("Invalid input. Try again.");
-        return;
+        return 0;
     }
 
-    printf("Output: %s", *ptr);
+    printf("Output: %s\n", ptr);
 
 }
