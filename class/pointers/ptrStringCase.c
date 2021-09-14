4a33cea4743925a97e2d7cb14a01b58582dd2c04
Rahul Bansal <rahulbansal.btech20@sushantuniversity.edu.in>
2021-09-14 22:25:51 +0530

    done

--- class/pointers/ptrStringCase.c
+++ class/pointers/ptrStringCase.c
@@ -1,6 +1,9 @@
 // Write a program to change the entire case of the string to lower/upper.
 
+//Expansion - Toggle case, Sentence case, Title Case
+
 #include <stdio.h>
+
 
 void toLowerCase(char *s) {
 
@@ -17,40 +20,109 @@ void toUpperCase(char *s) {
     for(int i=0; s[i] != '\0'; i++) {
 
         if('a' <= s[i] && 'z' >= s[i]) 
-        s[i] = s[i] - ('a' - 'A');
+            s[i] = s[i] - ('a' - 'A');
     
     }
 }
 
+void toToggleCase(char *s) {
+
+    for(int i=0; s[i] != '\0'; i++) {
+
+        if('a' <= s[i] && 'z' >= s[i]) 
+            s[i] = s[i] - ('a' - 'A');
+    
+        else if('A' <= s[i] && 'Z' >= s[i])
+            s[i] = s[i] + ('a' - 'A');
+    }
+}
+
+void toTitleCase(char *s) {
+
+    if('a' <= s[0] && 'z' >= s[0]) 
+            s[0] = s[0] - ('a' - 'A');
+
+    for(int i=0; s[i] != '\0'; i++) {
+
+        if(s[i] == ' ') {
+            
+            i++;
+
+            if('a' <= s[i] && 'z' >= s[i]) 
+            s[i] = s[i] - ('a' - 'A');
+        }
+        
+    }
+}
+
+void toSentenceCase(char *s) {
+
+    if('a' <= s[0] && 'z' >= s[0]) 
+            s[0] = s[0] - ('a' - 'A');
+
+    for(int i=0; s[i] != '\0'; i++) {
+
+        if(s[i] == '.' || s[i] == '!' || s[i] == '?') {
+            
+            i+=2;
+
+            if('a' <= s[i] && 'z' >= s[i]) 
+            s[i] = s[i] - ('a' - 'A');
+        }
+    }
+}
+
+int caseChoice(char *ptr) {
+
+    int choice;
+
+    printf("\n\nInput '1' for Upper Case.");
+    printf("\nInput '2' for Lower Case.");
+    printf("\nInput '3' for Toggle Case.");
+    printf("\nInput '4' for Title Case.");
+    printf("\nInput '5' for Sentence Case.");
+
+    printf("\n\nInput: ");
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        toUpperCase(ptr);
+    }
+    else if (choice == 2) {
+        toLowerCase(ptr);
+    }
+    else if (choice == 3) {
+        toToggleCase(ptr);
+    }
+    else if (choice == 4) {
+        toTitleCase(ptr);
+    }
+    else if (choice == 5) {
+        toSentenceCase(ptr);
+    }
+    else {
+        printf("Invalid input. Try again.");
+        caseChoice(choice);
+    }
+
+}
+
 int main() {
 
-    char str[30], *ptr;
+    char str[120], *ptr;
     ptr = str;
 
     int fnc;
 
     printf("Enter a string: ");
-    scanf("%s", str);
+    scanf("%[^\n]s", str);
 
     printf("\nInput: %s", ptr);
 
-    printf("\n\nWould you like to convert the string to lower case or upper case?");
-    printf("\nInput '1' for Upper Case and '2' for Lower Case.");
-    printf("\nInput :");
-
-    scanf("%d", &fnc);
-
-    if (fnc == 1) {
-        toUpperCase(str);
-    }
-    else if (fnc == 2) {
-        toLowerCase(str);
-    }
-    else {
-        printf("Invalid input. Try again.");
-        return 0;
-    }
+    caseChoice(ptr);
 
     printf("Output: %s\n", ptr);
+
+    return 0;
 
 }
